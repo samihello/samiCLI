531d35ac761f5e58e9d0b1b31282b6a4f52c39d9
Samantha Huynh <samantha.huynh@qlik.com>
2024-11-21 10:24:29 +0100

    Add command: random

---
+++ cmd/random.go
@@ -0,0 +1,34 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// randomCmd represents the random command
+var randomCmd = &cobra.Command{
+	Use:   "random",
+	Short: "Returns a random joke",
+	Long:  `Command 'random' gives you a random joke in the terminal.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("random called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(randomCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// randomCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// randomCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
