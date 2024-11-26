e547c43fa0c31d99ecb80e126290d957f32daef7
Samantha Huynh <samantha.huynh@qlik.com>
2024-11-26 13:20:25 +0100

    Simulate changes (and no changes)

--- scripts/main.go
+++ scripts/main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello, Sami!")
+	fmt.Println()
+	fmt.Println("Simulating compile and bundle of API specifications...")
+	fmt.Println("(script by Anders in api/build/main.go)")
+	fmt.Println("...by copying content to to trigger a delta/change in API spec.")
+	fmt.Println()
+
+	output()
+}
+
+func output() {
+	sourceFileWithChanges := "../api/docs.yaml"
+	sourceFileWithoutChanges := "../api/no-changes.yaml"
+	fmt.Println("Source file with changes: ", sourceFileWithChanges)
+	fmt.Println("Source file without changes: ", sourceFileWithoutChanges)
+
+	sourceFile := sourceFileWithoutChanges
+	destinationFile := "../api/resources-api-docs.yaml"
+
+	data, err := ioutil.ReadFile(sourceFile)
+	if err != nil {
+		log.Fatalf("Failed to read source file: %v", err)
+	}
+
+	destDataBefore, err := ioutil.ReadFile(destinationFile)
+	if err != nil {
+		log.Fatalf("Failed to read destination file before copy: %v", err)
+	}
+
+	err = ioutil.WriteFile(destinationFile, data, 0644)
+	if err != nil {
+		log.Fatalf("Failed to write to destination file: %v", err)
+	}
+
+	destDataAfter, err := ioutil.ReadFile(destinationFile)
+	if err != nil {
+		log.Fatalf("Failed to read destination file after copy: %v", err)
+	}
+
+	fmt.Println("---Simulation complete.")
+	if string(destDataBefore) == string(destDataAfter) {
+		fmt.Println("No changes detected in API specification.")
+	} else {
+		fmt.Println("Changes detected in API specification.")
+	}
+	fmt.Println()
 }
